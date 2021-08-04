1ed258e4e0768c4b2be03dc83768426aa3c4fba5
Lucas Regis de Moraes <LucasGRM03@gmail.com>
2021-08-04 08:26:26 -0300

    aula 4 terminada

--- main.js
+++ main.js
@@ -15,41 +15,56 @@ for (link of links) {
   });
 }
 
-// Header scroll
-const header = document.querySelector("#header");
-let navHeight = header.offsetHeight;
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-});
-
 // Swiper
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   slidesPerWiew: 1,
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
   },
   mousewheel: true,
-  keyboard: true
-})
+  keyboard: true,
+});
 
 // ScrollRevel: show element when scroll the page
 const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
+  origin: "top",
+  distance: "30px",
   duration: 700,
-  reset: true
-})
+  reset: true,
+});
 
 scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
-  `, 
-{interval:100}
-)
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+);
+
+// Header scroll Function
+function headerScroll() {
+  const header = document.querySelector("#header");
+  let navHeight = header.offsetHeight;
+
+  window.scrollY >= navHeight
+    ? header.classList.add("scroll")
+    : header.classList.remove("scroll");
+}
+
+// Back to top Function
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  window.scrollY >= 560
+    ? backToTopButton.classList.add("show")
+    : backToTopButton.classList.remove("show");
+}
+
+// When Scroll
+window.addEventListener("scroll", () => {
+  backToTop();
+  headerScroll();
+});
